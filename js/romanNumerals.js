5b737907088116e23946fb473465632586df12c2
Crusader-RG <justin.leeper92@gmail.com>
2024-05-26 13:50:30 -0600

    completed with object/while loop

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,26 @@
 exports.toRoman = function(num) {
+    let outputStr = ""
+    let romanNumeralToArabicMap = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1 
+    }
 
+    for (key in romanNumeralToArabicMap) {
+        while (num >= romanNumeralToArabicMap[key]) {
+            outputStr += key;
+            num -= romanNumeralToArabicMap[key];
+        }
+    }
+   return outputStr
 };
